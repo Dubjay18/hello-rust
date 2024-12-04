45f56a20d1b07c2c12c4ba15588204397067d1ea
Dubjay18 <jejeniyi7@gmail.com>
2024-12-04 12:28:25 +0100

    a1 done

--- ztm/activities/src/bin/a1.rs
+++ ztm/activities/src/bin/a1.rs
@@ -8,4 +8,15 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name() -> String {
+  return "John".to_string();
+
+}
+fn last_name() -> String {
+    return "Doe".to_string();
+}
+
+fn main() {
+    println!("First name: {}", first_name());
+    println!("Last name: {}", last_name());
+}
