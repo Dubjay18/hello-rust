bfd8a5ca174009958c14b6f003adf9a221cb2d53
Dubjay18 <jejeniyi7@gmail.com>
2024-12-05 00:31:37 +0100

    a11,a12 done

--- ztm/activities/src/bin/a11.rs
+++ ztm/activities/src/bin/a11.rs
@@ -9,4 +9,25 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GItem {
+    quantity: i32,
+    id: i32
+}
+
+
+fn display_quantity(item: &GItem) {
+    println!("{:?}",item.quantity)
+}
+
+fn display_id(g_item: &GItem) {
+    println!("{:?}",g_item.id)
+}
+fn main() {
+    let gi = GItem {
+        quantity: 10,
+        id: 110,
+    };
+    display_quantity(&gi);
+    display_id(&gi)
+
+}
--- ztm/activities/src/bin/a12.rs
+++ ztm/activities/src/bin/a12.rs
@@ -10,4 +10,68 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+
+enum Color {
+    Red,
+    Black,
+    BLue
+
+}
+
+impl Color {
+    fn print(&self) {
+       match self {
+            Color::Black => println!("black"),
+            Color::BLue => println!("blue"),
+            Color::Red => println!("red")
+        }
+
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("depth: {:?}", self.depth);
+
+    }
+}
+struct Box {
+    weight: f64,
+    dimensions: Dimensions,
+    color: Color
+}
+
+impl Box {
+    fn create(color: Color,dimensions:Dimensions,weight: f64) -> Self {
+        Self{
+            color,
+            dimensions,
+            weight
+        }
+    }
+
+    fn print_dets(&self) {
+      self.dimensions.print();
+        self.color.print();
+        println!("{}",self.weight)
+    }
+}
+
+
+fn main() {
+    let dimensions = Dimensions{
+        width: 12.0,
+        height: 13.0,
+        depth: 60.0
+    };
+    let new_box = Box::create(Color::Black,dimensions,60.7);
+    new_box.print_dets()
+}
